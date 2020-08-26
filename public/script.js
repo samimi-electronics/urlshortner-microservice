fa9923056ad3e8f37d933d7139c06ebf8046190b
Hamid Samimi <samimi.electronics@gmail.com>
2020-08-27 01:12:49 +0430

    front end response validation: better styling

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
+// Constants
 const inputURL = document.getElementById('url_input')
 const submitBtn = document.getElementById('submitBtn')
+const errorMsg = document.querySelector('.errorMsg')
 
+// Get the shortened URL from the database
 const getUrl = async (url) => {
     const response = await fetch('/api/shorturl/new', {
         method: 'POST',
@@ -15,19 +18,31 @@ const getUrl = async (url) => {
     return response.json()
 }
 
+/*
+* TODO: Show shortened url to user
+*  */
+// Function to display response to user
 const showResponse = (res) => {
     const baseApiUrl = 'api/shorturl'
     if (res.status === 400 || res.status === 404 || res.status === 500) {
         console.log(res.error)
         inputURL.className += ' inputError'
+        errorMsg.style.display = 'block'
     } else {
         console.log(`${window.location.href}${baseApiUrl}/${res.short_url}`)
+        errorMsg.style.display = ''
     }
 }
 
 submitBtn.addEventListener('click', async (e) => {
+    // Prevents default behavior: Redirect to response
     e.preventDefault()
     const responseData = await getUrl(inputURL.value);
     console.log(responseData)
     showResponse(responseData)
+})
+
+inputURL.addEventListener('input', () => {
+    inputURL.className = ''
+    errorMsg.style.display = 'none'
 })
